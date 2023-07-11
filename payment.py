1384a7a7bea92d83f1bb75acd481ddfe8f22e38f
andi598610 <andi598610@gmail.com>
2023-07-11 13:59:13 +0300

    correcting variable relationships

--- payment.py
+++ payment.py
@@ -13,7 +13,7 @@ def calculate_payment_per_unit(row, base_price, active_user_factor, task_age_fac
         base_price
         - active_user_factor * np.log(active_users + 1)
         + task_age_factor * np.log(task_age + 1)
-        - total_task_factor * np.log(total_tasks + 1)
-        + ether_price_factor * np.log(ether_price + 1)
+        + total_task_factor * np.log(total_tasks + 1)
+        - ether_price_factor * np.log(ether_price + 1)
     )
 
